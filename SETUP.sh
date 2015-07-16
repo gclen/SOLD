3594fd909154a4a7839cc36a0d91297e8e22f535
gclen <graham.clendenning@uoit.net>
2015-07-16 00:17:16 -0400

    Changed /usr/local/bin to /usr/bin

--- SETUP.sh
+++ SETUP.sh
@@ -10,7 +10,7 @@ make
 #Copy the gui files to the ase-gui directory
 cp levels.py gui.py /usr/lib/python2.7/dist-packages/ase/gui
 
-#Copy the executable C++ and python into /usr/local/bin
+#Copy the executable C++ and python into /usr/bin
 cp SOLD SOLD_plot.py cubegen /usr/bin
 
 #Go up one directory to the original one
